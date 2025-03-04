6c6e9eba9cba4e1b7b40754792d5cf906f75cdd5
aidensmith <aidenws715@gmail.com>
2025-03-04 10:13:29 -0500

    music for patrick day

--- assets/scripts/codeFrag.js
+++ assets/scripts/codeFrag.js
@@ -13,12 +13,12 @@ To add a code fragment to your page, use this format (with codeFragment, and loa
 
 function loadAudioVideo() { //<div id="audioVideo"><script src="../assets/scripts/codeFrag.js"></script><script>loadAudioVideo();</script></div>
     const homeButtonHTML = `
-        <audio id="audio" muted loop preload="auto">
-                    <source src="../assets/audio/audio.mp3" type="audio/mp3">
+        <audio id="audio" loop preload="auto">
+                    <source src="../assets/audio/Scents.mp3" type="audio/mp3">
                 </audio>
 
                 <video id="video" class="fullscreen bg-video"  loop playsinline autoplay preload="auto">
-                    <source src="../assets/images/tyler.mp4" type="video/mp4">
+                    <source src="../assets/images/patrick.mp4" type="video/mp4">
                 </video>
         `;
     document.getElementById('audioVideo').innerHTML = homeButtonHTML;
